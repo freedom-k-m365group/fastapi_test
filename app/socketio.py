79322ea14b0799349e8a27a17c25c119834a38ef
freedom-k-m365group <freedom.k@m365connect.com>
2025-10-28 12:28:09 +0300

    modification to handle async server setup correctly

--- app/socketio.py
+++ app/socketio.py
@@ -2,5 +2,5 @@ import socketio
 
 sio = socketio.AsyncServer(async_mode='asgi',
                            cors_allowed_origins='*',
-                           client_manager=socketio.RedisManager(
-                               'redis://localhost:6379'))
+                           client_manager=socketio.AsyncRedisManager(
+                               'redis://localhost:6379/0'))
